ed8a2417b85272eb1022f973c92c16e742bde383
Asman <iasman312@gmail.com>
2021-03-05 18:33:53 +0600

    Создали вьюшку для детяльного просмотра

--- shop/webapp/urls.py
+++ shop/webapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from webapp.views import index_view
+from webapp.views import (index_view, product_view,)
 
 urlpatterns = [
     path('', index_view, name='product-list'),
+    path('<int:pk>/', product_view, name='product-view'),
 ]
--- shop/webapp/views.py
+++ shop/webapp/views.py
@@ -6,3 +6,8 @@ from webapp.models import Product
 def index_view(request):
     products = Product.objects.all()
     return render(request, 'index.html', context={'products': products})
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, id=pk)
+    return render(request, 'product_view.html', context={'product': product})
