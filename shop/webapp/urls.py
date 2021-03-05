f93014439456d12607555937ec414775b0105cbf
Asman <iasman312@gmail.com>
2021-03-05 19:17:22 +0600

    создали вьюшку для редактирования товара

--- shop/webapp/urls.py
+++ shop/webapp/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from webapp.views import (index_view, product_view, product_create_view)
+from webapp.views import (
+    index_view,
+    product_view,
+    product_create_view,
+    product_update_view
+)
 
 urlpatterns = [
     path('', index_view, name='product-list'),
     path('add/', product_create_view, name='product-add'),
     path('<int:pk>/', product_view, name='product-view'),
+    path('<int:pk>/update', product_update_view, name='product-update'),
 ]
--- shop/webapp/views.py
+++ shop/webapp/views.py
@@ -30,3 +30,33 @@ def product_create_view(request):
             )
             return redirect('product-view', pk=article.id)
         return render(request, 'product_create.html', context={'form': form})
+
+
+def product_update_view(request, pk):
+    product = get_object_or_404(Product, id=pk)
+
+    if request.method == 'GET':
+        form = ProductForm(initial={
+            'title': product.title,
+            'content': product.content,
+            'author': product.author
+        })
+        return render(request, 'product_update.html', context={'form': form,
+                                                               'product':
+                                                                   product})
+
+    elif request.method == 'POST':
+        form = ProductForm(
+            data=request.POST)
+        if form.is_valid():
+            product.name = form.cleaned_data.get('name'),
+            product.description = form.cleaned_data.get('description'),
+            product.category = form.cleaned_data.get('category'),
+            product.balance = form.cleaned_data.get('balance'),
+            product.price = form.cleaned_data.get('price')
+            product.save()
+            return redirect('product-view',
+                            pk=product.id)
+
+        return render(request, 'product_create.html',
+                      context={'form': form, 'product': product})
