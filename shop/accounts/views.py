bbe387c03251a5c2d901dbabfb5a481e34a80761
asman <iasman312>
2021-04-30 14:44:49 +0600

    добавили представление при выходе добавлять количество товаров

--- shop/accounts/views.py
+++ shop/accounts/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.views import LogoutView
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from accounts.forms import MyUserCreationForm
+from webapp.models import Cart, Product
 
 
 def register_view(request, *args, **kwargs):
@@ -13,3 +15,16 @@ def register_view(request, *args, **kwargs):
     else:
         form = MyUserCreationForm()
     return render(request, 'user_create.html', context={'form': form})
+
+
+class MyLogoutView(LogoutView):
+    def dispatch(self, request, *args, **kwargs):
+        session = self.request.session.get('my_dict', [])
+        carts = Cart.objects.filter(pk__in=session)
+        if carts:
+            for cart in carts:
+                product = Product.objects.get(pk=cart.product.pk)
+                product.balance += cart.quantity
+                product.save()
+                cart.delete()
+        return super().dispatch(request, *args, **kwargs)
